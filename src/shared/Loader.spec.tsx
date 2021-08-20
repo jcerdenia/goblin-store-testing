2e78fd421bc7d86bd240ee413d644ace6554ad10
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-08-20 22:01:07 +0800

    Add Loader test

--- src/shared/Loader.spec.tsx
+++ src/shared/Loader.spec.tsx
@@ -2,6 +2,9 @@ import React from "react"
 import { Loader } from "./Loader"
 import { render } from "@testing-library/react"
 
+// Loader component contains no logic, so we just make
+// sure it renders correctly.
+
 describe("Loader", () => {
   it("renders correctly", () => {
     const { container } = render(<Loader />)
