2f362946c7753d16dc6b004ebffa714e7185cd53
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-08-20 21:54:47 +0800

    Add unit tests for CartWidget

--- src/shared/CartWidget/CartWidget.spec.tsx
+++ src/shared/CartWidget/CartWidget.spec.tsx
@@ -1,33 +1,32 @@
 import React from "react"
 import { CartWidget } from "./CartWidget"
-import { fireEvent } from "@testing-library/react"
+import { fireEvent, getByRole } from "@testing-library/react"
+
+// Cart Widget component displays the number of products in the cart.
+// The whole component is a link; clicking on it takes you to the cart summary page.
 
 describe("CartWidget", () => {
-  it("shows the amount of products in the cart", () => {
-    const stubCartHook = () => ({
-      products: [
-        {
-          name: "Product foo",
-          price: 0,
-          image: "image.png"
-        }
-      ],
-    })
 
-    const { container } = renderWithRouter(() => (
-      <CartWidget useCartHook={stubCartHook} />
-    ))
+  // We can plan out tests to write by using it.todo.
+  // This allows us to write the test case name without the callback.
 
-    expect(container.innerHTML).toMatch("1")
-  })
+  // it.todo("shows the amount of products in the cart")
+  // it.todo("navigates to cart summary page on click")
 
   it("navigates to cart summary page on click", () => {
-    const { getByRole, history } = renderWithRouter(() => (
-      <CartWidget />
-    ))
-
+    // getByRole selector uses the aria-role attr to find the element.
+    const { getByRole, history } = renderWithRouter(() => (<CartWidget />))
+    // Click the element then check if we end up on the /cart route.
     fireEvent.click(getByRole("link"))
-    
     expect(history.location.pathname).toEqual("/cart")
+  })
+
+  it("shows the amount of products in the cart", () => {
+    const stubCartHook = () => ({
+      products: [{ name: "Product foo", price: 0, image: "image.png" }]
+    })
+
+    const { container } = renderWithRouter(() => (<CartWidget useCartHook={stubCartHook} />))
+    expect(container.innerHTML).toMatch("1")
   })
 })
