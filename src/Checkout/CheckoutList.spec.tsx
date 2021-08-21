dcbc30b694e48db482a8784737aa7e5627122b12
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-08-21 19:47:21 +0800

    Add and pass checkoutList test

--- src/Checkout/CheckoutList.spec.tsx
+++ src/Checkout/CheckoutList.spec.tsx
@@ -3,24 +3,14 @@ import { CheckoutList } from "./CheckoutList"
 import { Product } from "../shared/types"
 import { render } from "@testing-library/react"
 
+// Only one task! To render a list of products.
+
 describe("CheckoutList", () => {
   it("renders list of products", () => {
-    const products: Product[] = [
-      {
-        name: "Product foo",
-        price: 10,
-        image: "/image.png"
-      },
-      {
-        name: "Product bar",
-        price: 10,
-        image: "/image.png"
-      }
-    ]
+    const products: Product[] = ["Product foo", "Product bar"]
+      .map((name) => ({ name: name, price: 10, image: "/image.png" }))
 
-    const { container } = render(
-      <CheckoutList products={products} />
-    )
+    const { container } = render(<CheckoutList products={products} />)
     expect(container.innerHTML).toMatch("Product foo")
     expect(container.innerHTML).toMatch("Product bar")
   })
