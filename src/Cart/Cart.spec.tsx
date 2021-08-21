d7731ef12e7415452827ebca237c227e704a3f37
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-08-21 17:09:14 +0800

    Add and pass Cart tests

--- src/Cart/Cart.spec.tsx
+++ src/Cart/Cart.spec.tsx
@@ -3,45 +3,34 @@ import { Cart } from "./Cart"
 import { fireEvent } from "@testing-library/react"
 import { CartItemProps } from "./CartItem"
 
+// The Cart component has two possible results:
+// [1]  Product array is empty: 
+//      - renders "empty cart" message with the link to the products page
+//      - clicking the link redirects to /
+// [2]  Cart has products
+//      - render the list of products, total price, "Go to checkout" button
+//      - clicking the button redirects to /checkout
+
 jest.mock("./CartItem", () => ({
   CartItem: ({ product }: CartItemProps) => {
     const { name, price, image } = product
-    return (
-      <div>
-        {name} {price} {image}
-      </div>
-    )
+    return (<div>{name} {price} {image}</div>)
   }
 }))
 
 describe("Cart", () => {
   describe("without products", () => {
-    const stubCartHook = () => ({
-      products: [],
-      removeFromCart: () => {},
-      totalPrice: () => 0
-    })
+    const stubCartHook = () => ({ products: [], removeFromCart: () => {}, totalPrice: () => 0 })
 
     it("renders empty cart message", () => {
-      const { container } = renderWithRouter(() => (
-        <Cart useCartHook={stubCartHook} />
-      ))
-      expect(container.innerHTML).toMatch(
-        "Your cart is empty."
-      )
+      const { container } = renderWithRouter(() => <Cart useCartHook={stubCartHook} />)
+      expect(container.innerHTML).toMatch("Your cart is empty.")
     })
 
     describe("on 'Back to main page' click", () => {
-      it("redirects to '/'", () => {
-        const {
-          getByText,
-          history
-        } = renderWithRouter(() => (
-          <Cart useCartHook={stubCartHook} />
-        ))
-
+      it("redirects to /", () => {
+        const { getByText, history } = renderWithRouter(() => <Cart useCartHook={stubCartHook} />)
         fireEvent.click(getByText("Back to main page."))
-
         expect(history.location.pathname).toBe("/")
       })
     })
@@ -49,49 +38,23 @@ describe("Cart", () => {
 
   describe("with products", () => {
     const products = [
-      {
-        name: "Product foo",
-        price: 100,
-        image: "/image/foo_source.png"
-      },
-      {
-        name: "Product bar",
-        price: 100,
-        image: "/image/bar_source.png"
-      }
+      { name: "Product foo", price: 100, image: "/image/foo_source.png" },
+      { name: "Product bar", price: 100, image: "/image/bar_source.png" }
     ]
 
-    const stubCartHook = () => ({
-      products,
-      removeFromCart: () => {},
-      totalPrice: () => 55
-    })
+    const stubCartHook = () => ({ products, removeFromCart: () => {}, totalPrice: () => 55 })
 
     it("renders cart products list with total price", () => {
-      const { container } = renderWithRouter(() => (
-        <Cart useCartHook={stubCartHook} />
-      ))
-
-      expect(container.innerHTML).toMatch(
-        "Product foo 100 /image/foo_source.png"
-      )
-      expect(container.innerHTML).toMatch(
-        "Product bar 100 /image/bar_source.png"
-      )
+      const { container } = renderWithRouter(() => <Cart useCartHook={stubCartHook} />)
+      expect(container.innerHTML).toMatch("Product foo 100 /image/foo_source.png")
+      expect(container.innerHTML).toMatch("Product bar 100 /image/bar_source.png")
       expect(container.innerHTML).toMatch("Total: 55 Zm")
     })
 
     describe("on 'go to checkout' click", () => {
-      it("redirects to '/checkout'", () => {
-        const {
-          getByText,
-          history
-        } = renderWithRouter(() => (
-          <Cart useCartHook={stubCartHook} />
-        ))
-
+      it("redirects to '/checkout", () => {
+        const { getByText, history } = renderWithRouter(() => <Cart useCartHook={stubCartHook} />)
         fireEvent.click(getByText("Go to checkout"))
-
         expect(history.location.pathname).toBe("/checkout")
       })
     })
