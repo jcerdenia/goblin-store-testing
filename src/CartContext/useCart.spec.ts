1f95e4be988472a5b63fa6f527e656cde40e1970
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-08-22 14:05:04 +0800

    Add and pass useCart hook tests

--- src/CartContext/useCart.spec.ts
+++ src/CartContext/useCart.spec.ts
@@ -1,142 +1,76 @@
-import { useCart } from "./useCart"
-import { renderHook } from "@testing-library/react-hooks"
+import React from "react"
 import { Product } from "../shared/types"
-import TestRenderer from "react-test-renderer"
-const { act } = TestRenderer
+import { renderHook } from "@testing-library/react-hooks"
+import { useCart } from "./useCart"
+import { act } from "@testing-library/react"
+
+// useCart hook allows us to get the list of products in the cart,
+// add new products, or clear the cart.
 
 const localStorageMock = (() => {
   let store: { [key: string]: string } = {}
   return {
-    clear: () => {
-      store = {}
-    },
-    getItem: (key: string) => {
-      return store[key] || null
-    },
-    removeItem: (key: string) => {
-      delete store[key]
-    },
-    setItem: jest.fn((key: string, value: string) => {
-      store[key] = value ? value.toString() : ""
-    })
+    clear: () => store = {},
+    getItem: (key: string) => store[key] || null,
+    removeItem: (key: string) => delete store[key],
+    setItem: jest.fn((key: string, value: string) => store[key] = value ? value.toString() : "")
   }
-})()
+})() // don't forget to call it
 
-Object.defineProperty(window, "localStorage", {
-  value: localStorageMock
-})
+// Assign mock local storage on the window object:
+Object.defineProperty(window, "localStorage", { value: localStorageMock })
 
 describe("useCart", () => {
-  afterEach(() => {
-    localStorageMock.clear()
-  })
+  afterEach(() => localStorageMock.clear())
 
   describe("on mount", () => {
-    it("it loads data from localStorage", () => {
-      const products: Product[] = [
-        {
-          name: "Product foo",
-          price: 0,
-          image: "image.jpg"
-        }
-      ]
-      localStorageMock.setItem(
-        "products",
-        JSON.stringify(products)
-      )
-
+    it("loads data from localStorage", () => {
+      const products: Product[] = [{ name: "Product foo", price: 0, image: "image.jpg" }]
+      localStorageMock.setItem("products", JSON.stringify(products))
       const { result } = renderHook(useCart)
-
       expect(result.current.products).toEqual(products)
     })
   })
 
+  // NB: naming convention from RSpec to to call function tests with # prefix
   describe("#addToCart", () => {
     it("adds item to the cart", () => {
-      const product: Product = {
-        name: "Product foo",
-        price: 0,
-        image: "image.jpg"
-      }
+      const product: Product = { name: "Product foo", price: 0, image: "image.jpg" }
       const { result } = renderHook(useCart)
-
-      act(() => {
-        result.current.addToCart(product)
-      })
-
+      act(() => result.current.addToCart(product)) // remember to use act because we alter the state of the hook
       expect(result.current.products).toEqual([product])
-      expect(localStorageMock.setItem).toHaveBeenCalledWith(
-        "products",
-        JSON.stringify([product])
-      )
+      expect(localStorageMock.setItem).toHaveBeenCalledWith("products", JSON.stringify([product]))
     })
   })
 
-  describe("#removeFromCart", () => {
+  describe("removeFromCart", () => {
     it("removes item from the cart", () => {
-      const product: Product = {
-        name: "Product foo",
-        price: 0,
-        image: "image.jpg"
-      }
-      localStorageMock.setItem(
-        "products",
-        JSON.stringify([product])
-      )
-
+      const product: Product = { name: "Product foo", price: 0, image: "image.jpg" }
+      localStorageMock.setItem("products", JSON.stringify([product]))
       const { result } = renderHook(useCart)
-
-      act(() => {
-        result.current.removeFromCart(product)
-      })
-
+      act(() => result.current.removeFromCart(product))
       expect(result.current.products).toEqual([])
-      expect(localStorageMock.setItem).toHaveBeenCalledWith(
-        "products",
-        "[]"
-      )
+      expect(localStorageMock.setItem).toHaveBeenCalledWith("products", "[]")
     })
   })
 
   describe("#totalPrice", () => {
     it("returns total products price", () => {
-      const product: Product = {
-        name: "Product foo",
-        price: 21,
-        image: "image.jpg"
-      }
-      localStorageMock.setItem(
-        "products",
-        JSON.stringify([product, product])
-      )
+      const product: Product = { name: "Product foo", price: 21, image: "image.jpg" }
+      localStorageMock.setItem("products", JSON.stringify([product, product])) // we pass the product twice
       const { result } = renderHook(useCart)
-
-      expect(result.current.totalPrice()).toEqual(42)
+      expect(result.current.totalPrice()).toEqual(42) // 21 * 2
     })
   })
 
   describe("#clearCart", () => {
     it("removes all the products from the cart", () => {
-      const product: Product = {
-        name: "Product foo",
-        price: 21,
-        image: "image.jpg"
-      }
-      localStorageMock.setItem(
-        "products",
-        JSON.stringify([product, product])
-      )
+      const product: Product = { name: "Product foo", price: 21, image: "image.jpg" }
+      localStorageMock.setItem("products", JSON.stringify([product, product]))
       const { result } = renderHook(useCart)
-
-      act(() => {
-        result.current.clearCart()
-      })
-
+      act(() => result.current.clearCart())
       expect(result.current.products).toEqual([])
-      expect(localStorageMock.setItem).toHaveBeenCalledWith(
-        "products",
-        "[]"
-      )
+      expect(localStorageMock.setItem).toHaveBeenCalledWith("products", "[]")
     })
   })
 })
